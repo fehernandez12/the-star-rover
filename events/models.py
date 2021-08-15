b7e8d0de5c40d942e1093c334aee698e91ffe728
Felipe Hernández <feehernandezba@gmail.com>
2021-08-15 13:24:49 -0500

    Added artist models

--- events/models.py
+++ events/models.py
@@ -1,3 +1,80 @@
-from django.db import models
+import uuid
+from datetime import date
 
+from django.db import models
+from django_countries.fields import CountryField
+
+from employees.models import DirectiveEmployee, RegularEmployee
+# Dependency Injection
+SOLO_GROUP = (
+    ('soloist', 'Soloist'),
+    ('group', 'Group'),
+)
 # Create your models here.
+class Event(models.Model):
+    """
+    Event model. Each event will feature multiple parades.
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(db_index=True)
+    start_date = models.DateField(default=date.today)
+    end_date = models.DateField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    directive = models.ForeignKey(
+        DirectiveEmployee,
+        on_delete=models.CASCADE,
+        related_name='event_directive'
+    )
+    employees = models.ManyToManyField(RegularEmployee, related_name='event_employees')
+
+    class Meta:
+        ordering = ['-created']
+
+class Designer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    passport = models.CharField(max_length=20)
+    country = CountryField()
+
+    class Meta:
+        ordering = ['last_name']
+
+class Parade(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='parade_event')
+    designer = models.ForeignKey(Designer, on_delete=models.CASCADE, related_name='parade_designer')
+    date = models.DateField()
+    time = models.TimeField()
+    collection = models.CharField(max_length=256)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    ended = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['date']
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ['name']
+
+class Artist(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    artist_name = models.CharField(max_length=256)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, related_name='artist_genre')
+    contract = models.BooleanField(default=False)
+    salary = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ['artist_name']
+
+class Group(models.Model):
+    artist = models.OneToOneField(Artist, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+class Soloist(models.Model):
+    artist = models.OneToOneField(Artist, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, models.DO_NOTHING, blank=True, null=True)
--- models/models.py
+++ models/models.py
@@ -1,3 +1,60 @@
+import uuid
+from django_countries.fields import CountryField
+
 from django.db import models
 
+# Dependency injection
+EYE_COLORS = (
+    ('green', 'Green'),
+    ('blue', 'Blue'),
+    ('brown', 'Brown'),
+    ('hazel', 'Hazel'),
+    ('gray', 'Gray'),
+    ('dark', 'Dark'),
+    ('other', 'Other'),
+)
+SKIN_COLORS = (
+    ('black', 'Black'),
+    ('brown', 'Brown'),
+    ('olive', 'Olive'),
+    ('fair', 'Fair'),
+    ('pale', 'Pale'),
+)
 # Create your models here.
+class ModelAgency(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=256)
+    country = CountryField()
+    creation_year = models.DateField()
+    email = models.EmailField()
+    owner_name = models.CharField(max_length=512)
+    parent_agency = models.ForeignKey('self', models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        ordering = ['name']
+
+class Measurements(models.Model):
+    chest = models.SmallIntegerField()
+    waist = models.SmallIntegerField()
+    hips = models.SmallIntegerField()
+
+class Model(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    country = CountryField()
+    birthday = models.DateField()
+    eye_color = models.CharField(choices=EYE_COLORS, max_length=10)
+    skin_color = models.CharField(choices=SKIN_COLORS, max_length=20)
+    height = models.SmallIntegerField()
+    shoe_size = models.SmallIntegerField()
+    weight = models.SmallIntegerField()
+    particularities = models.TextField()
+    measurements = models.ForeignKey(Measurements, on_delete=models.DO_NOTHING, related_name='model_measurements')
+    active = models.BooleanField(default=True)
+    salary = models.PositiveIntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['last_name']
